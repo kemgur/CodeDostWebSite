eab30e033c73eecf106e4bac4c1c27a6ec1a8b18
kemgur <67036379+kemgur@users.noreply.github.com>
2021-03-03 17:07:42 -0500

    Update AlphabetPattern01.java

--- src/sec01alphabetpatterns/AlphabetPattern01.java
+++ src/sec01alphabetpatterns/AlphabetPattern01.java
@@ -5,11 +5,8 @@ import java.util.Scanner;
 //Guzel bir soru
 //Cozunce daha guzel oldu.Remote olarak.  
 //Kemal
-<<<<<<< HEAD
 //Cakisma- kemal
-=======
-// cakisma- hamza (conflict)
->>>>>>> refs/remotes/origin/main
+
 public class AlphabetPattern01 {
 
 	/*
