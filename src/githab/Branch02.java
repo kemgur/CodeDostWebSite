e2d4d6505434fec014fc85defa5d52c8a695dee3
kemalgurler <kemgur52@gmail.com>
2021-03-03 23:12:43 -0500

    kemal branch1

--- src/githab/Branch02.java
+++ src/githab/Branch02.java
@@ -3,7 +3,9 @@ package githab;
 public class Branch02 {
 
 	public static void main(String[] args) {
-		
+		System.out.println("kemal branch i is e ele atiyor");
+		System.out.println("kemal branch i is e ele atiyor");
+		System.out.println("kemal branch i is e ele atiyor");
 
 	}
 
